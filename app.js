355bbf99e396e70b7267cabd73e3df784294ef33
Anita Jose <anitajose119@gmail.com>
2022-08-05 18:48:53 -0400

    inquirer imported and prompt created

--- app.js
+++ app.js
@@ -13,24 +13,38 @@
      executes that function on each element of the array, using the value of the element at that iteration as its argument. */
 // }
 
+// This array holds the user command-line arguments
+// const profileDataArgs = process.argv.slice(2)
+// const [name, gitHub] = profileDataArgs
+
+// imports inquirer
+const inquirer = require("inquirer")
+// check if inquirer got imported
+// console.log(inquirer);
+
+inquirer
+  .prompt([
+    {
+    type: "input",
+    name: "name",
+    message: "What is your name?"
+  }
+  ])
+  .then(answers => console.log(answers));
 
 // imports the fs module in to the app and puts it in a variable
-const fs = require("fs")
-
+// const fs = require("fs")
 // imports the generatePage function from the page-template.js file
-const generatePage = require("./src/page-template.js")
+// const generatePage = require("./src/page-template.js")
 
-// This array holds the user command-line arguments
-const profileDataArgs = process.argv.slice(2)
-const [name, gitHub] = profileDataArgs
-
-const pageHTML = generatePage(name, gitHub)
+// const pageHTML = generatePage(name, gitHub)
 
 // creates the actual html file using the template created and arguments passed through terminal
-fs.writeFile("./index.html", pageHTML, err => {
-    if (err) throw new Error(err)
-    console.log("Portfolio complete! Check out index.html to see the output!");
-})
+// fs.writeFile("./index.html", pageHTML, err => {
+//     if (err) throw new Error(err);
+
+//     console.log("Portfolio complete! Check out index.html to see the output!");
+// })
 
 // creates src folder
 // fs.mkdir("./src", (err) => {
@@ -40,4 +54,10 @@ fs.writeFile("./index.html", pageHTML, err => {
 // // created page-template file in src folder
 // fs.writeFile("./src/page-template.js", err => {
 //     if (err) throw err
+// })
+
+// // create .gitignore file
+// fs.writeFile("./.gitignore", "node_modules", (err) => {
+//     if (err) throw err;
+//     console.log(".gitignore file created!");
 // })
